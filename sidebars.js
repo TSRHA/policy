9ba1eed10ee29062bdbca483139f481b8fa2b850
Jacob Lee <jacobleegithub@users.noreply.github.com>
2026-07-20 10:22:53 +0200

    docs: strengthen white paper with Fable review

--- sidebars.js
+++ sidebars.js
@@ -11,6 +11,7 @@ const sidebars = {
         'appendix/case-comparison',
         'appendix/methodology',
         'appendix/consultation-log',
+        'appendix/policy-alignment',
       ],
     },
   ],
